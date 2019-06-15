0cd5c9947b741e782b900b9d5a41c706ad0dc2ce
Abhijit Roy <dalvath3700@gmail.com>
2019-06-15 15:43:00 +0530

    Final code with Button based GUI on Excel

--- Final_Excel/Final_Excel.py
+++ Final_Excel/Final_Excel.py
@@ -1,18 +1,17 @@
 import xlwings as xw
 import pandas as pd
+import win32api
 
 
 """
 Follow the steps:
 -----------------
 - parse the sheet1 from excel into df1
-- parse the sheet2 from excel into df2
-- take df1's 1st row as array
-- take df2's 1st row as array
-- compare(df1, df2)
-    + if (equal) ==> same in dfnew
+- parse the sheet2 from excel into df_default
+- compare(df1, df_default): loop across the df1's cells:
     + if (unequal) ==> keep default value in dfnew
-    + if (blank) ==> keep blank in dfnew
+        + if (blank) ==> keep blank in dfnew
+        + else ==> df1[i,j] = df_default[j]
 """
 
 
@@ -28,12 +27,12 @@ excel_file_directory = "I:\\github_repos\\My_Freelancing_Projects\\work-4\\data\
 
 #================================================================MAIN================================================================
 def main():
-    # wb = xw.Book.caller()
+    wb = xw.Book.caller()
     # wb.sheets[0].range("A1").value = "Hello xlwings!"     # for testing
 
     #------------------------------------------------------------------------------------------------
     # Sheets
-    # sht_main = wb.sheets['Main']
+    sht_main = wb.sheets['Main']
     excel_file = pd.ExcelFile(excel_file_directory)
 
 
@@ -41,8 +40,6 @@ def main():
     # dataframe- 'df_default'
     df_default = excel_file.parse(sht_name_default, skiprows= 0, header= None)      # Fetch without any column header
     df_default.columns = ['sub_category', 'value']
-    # sht_main.clear()   # Clear the content and formatting before displaying the data
-    # sht_main.range('A1').options(index=False).value = df_default        # show the dataframe (df_default) values into sheet- 'Main'
 
 
     #------------------------------------------------------------------------------------------------
@@ -50,10 +47,13 @@ def main():
     df1 = excel_file.parse(sht_name_data, skiprows= 1)
     sht_data_columns = df_default['sub_category'].tolist()
     df1 = df1[sht_data_columns]
+    # sht_main.clear()   # Clear the content and formatting before displaying the data
+    # sht_main.range('A1').options(index=False).value = df1        # show the dataframe (df1) values into sheet- 'Main'
+
     # print(df1)    # display 'df1'(float by default)
     df1.fillna(0, inplace=True)     # replace 'NaN' with zero so as to convert all into integer.
     df1 = df1.astype(int)       # convert 'df1' values (float by default) as int.
-    print(df1)    # display 'df1'(all converted into int)
+    # print(df1)    # display 'df1'(all converted into int)
     # sht_main.clear()   # Clear the content and formatting before displaying the data
     # sht_main.range('A1').options(index=False).value = df1        # show the dataframe (df1) values into sheet- 'Main'
 
@@ -70,8 +70,9 @@ def main():
     df1_index0 = df1.iloc[0].tolist()
 
 
-    if (df1_column_headers == df_default_subcategory) : 
-        print("Equal sub-categories in both the sheets!")     # check if both sheets contain the same sub-categories.
+    if (df1_column_headers == df_default_subcategory) :       # check if both the columns of 'sheet1' and index labels of 'sheet2' are same
+        # print("Equal sub-categories in both the sheets!")     # check if both sheets contain the same sub-categories.
+        win32api.MessageBox(wb.app.hwnd, "Sub-categories in both the sheets are equal!", "Check Sub-category", )
         # Loop in 'df1' and compare values with default value list
         for i in range(len_index):               # i= [0, 1, 2,...., 293]
             for j in range(len_subcategory):    # j= [0, 1, 2,...., 18]
@@ -80,24 +81,21 @@ def main():
                         df1.iloc[i,j] = 0
                     else:
                         df1.iloc[i,j] = df_default_value[j]
-                
+    
+        # Display the final dataframe
+        df1.replace(to_replace= 0, value= '', inplace=True) 
+        sht_main.clear()        # Clear the content and formatting before displaying the data
+        sht_main.range('A1').options(index=False).value = df1        # show the dataframe (final df1) values into sheet- 'Main'
 
     else:
-        print("Sorry! Please correct your sub-categories.")
+        # print("Sorry! Please correct your sub-categories.")
+        win32api.MessageBox(wb.app.hwnd, "Sorry! Please correct your sub-categories.", "Check Sub-category", )
+        sht_main.clear()        # Clear the content and formatting before displaying the data
 
-    #------------------------------------------------------------------------------------------------
-    # Display the final dataframe
-    # print(df1_column_headers)
-    # print(df_default_subcategory)
-    # print(df1_index0)
-    print(df_default['value'].tolist())
-    df1.replace(to_replace= 0, value= 'NaN', inplace=True) 
-    print(df1)
-    # print(df_default)
 
 
 # ===============================================================RUN MAIN Function==============================================================
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
 
 
